4114744e99814c0710152f2d31d83e1d7529ca4f
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-07-01 23:11:03 -0700

    add

--- JS/one_page_website.js
+++ JS/one_page_website.js
@@ -1,4 +1,4 @@
-<script>
+
 // Open the Modal
 
 function openModal() {
@@ -40,4 +40,3 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-</script>
